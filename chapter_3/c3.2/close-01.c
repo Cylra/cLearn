6ffa11d51945fd7b078bb7d206faf9968117a3fa
cc <long998127@gmail.com>
2017-09-08 23:47:42 +0800

    write close file

---
+++ chapter_3/c3.2/close-01.c
@@ -0,0 +1,24 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    int fd = 0;
+    for(i=0; fd>=0; i++)
+    {
+        fd = open("test.txt", O_RDONLY);
+        if(fd > 0){
+            printf("fd:%d\n", fd);
+            close(fd);
+        }
+        else{
+            printf("error, can't open file\n");
+            exit(0);
+        }
+    }
+    return 0;
+}
